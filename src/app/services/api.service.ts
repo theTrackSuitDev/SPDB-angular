9939fb73eb3ec59af808f9e417dc3be59eb6d098
theTrackSuitDev <dimiro89@gmail.com>
2024-03-14 23:21:04 +0200

    Added methods to get all projects and to create one

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
+import { Project } from '../types/project';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,15 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  getAllProjects(){
+    const { apiUrl } = environment;
+    return this.http.get<Project[]>(`${environment.apiUrl}/themes`);
+  }
 
+  createProject(projectName: string, description: string, imageUrl: string, videoUrl: string) {
+    const { apiUrl } = environment;
+    const payload = { projectName, description, imageUrl, videoUrl };
+
+    return this.http.post<Project>(`${apiUrl}/themes`, payload);
+  }
 }

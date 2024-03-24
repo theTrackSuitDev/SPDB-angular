9d70d52f9905e914af5cb1812735111b421f971d
theTrackSuitDev <dimiro89@gmail.com>
2024-03-24 12:13:39 +0200

    Updated types

--- src/app/types/project.ts
+++ src/app/types/project.ts
@@ -2,12 +2,14 @@ import { User } from "./user";
 
 export interface Project {
   subscribers: string[];
-  posts: string[];
+  posts: Comment[];
   _id: string;
   projectName: string;
+  technology: string;
   description: string;
   imageUrl: string;
   videoUrl: string;
+  gitHubRepo: string;
   userId: User;
   created_at: string;
   updatedAt: string;

307563076523a9aff40ca243fc36e7948996fba5
theTrackSuitDev <dimiro89@gmail.com>
2024-04-02 11:56:08 +0300

    Updated edit project view

--- src/app/modules/projects/edit-project/edit-project.component.ts
+++ src/app/modules/projects/edit-project/edit-project.component.ts
@@ -4,17 +4,20 @@ import { fadeInRightOnEnterAnimation } from 'angular-animations';
 import { imageUrlValidator } from '../../shared/utils/imageUrlValidator';
 import { videoUrlValidator } from '../../shared/utils/videoUrlValidator';
 import { gitUrlValidator } from '../../shared/utils/gitUrlValidator';
+import { ApiService } from 'src/app/services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-project',
   templateUrl: './edit-project.component.html',
-  styleUrls: ['./edit-project.component.css']
+  styleUrls: ['./edit-project.component.css'],
+  animations: [fadeInRightOnEnterAnimation()]
 })
 export class EditProjectComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService, private router: Router) {}
 
   form = this.formBuilder.group({
-    projectName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+    projectName: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]],
     technology: ['', [Validators.required]],
     description: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(50)]],
     imageUrl: ['', [Validators.required, imageUrlValidator()]],
